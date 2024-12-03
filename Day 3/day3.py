be6451dcb1cd3cdc8e9394d5cf3c0c5ebca5c944
Erica Hill <psyeh3@nottingham.ac.uk>
2024-12-03 15:09:02 +0000

    Completed day 3

---
+++ Day 3/day3.py
@@ -0,0 +1,29 @@
+import re
+
+file = "day3input.txt"
+
+input = open(file, "r")
+
+correctMul = re.findall(r"mul\(\d+,\d+\)", input.read()) #gets all correctly formatted muls
+
+count = 0
+for line in correctMul:
+    numbers = list(map(int, re.findall(r'\d+', line))) #gets only the integers from each line
+    count += numbers[0] * numbers[1]
+
+print("All added up multiplications: " + str(count)) #Answer to part 1
+
+doDontMul = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", open(file).read()) #gets all correctly formatted muls, do's, and don't's in text
+
+count=0
+process = True
+for line in doDontMul:
+    if line == "do()":
+        process = True
+    elif line == "don't()":
+        process = False
+    elif process == True: #only process muls with the latest do
+        numbers = list(map(int, re.findall(r'\d+', line))) #gets only the integers from each line
+        count += numbers[0] * numbers[1]
+        
+print("All added up multiplications with conditionals: " + str(count))
